bac27dd463e73590ffe2729c82cbd9b9287c225c
Licaster0 <ahmet0434recep@gmail.com>
2024-12-21 01:16:25 +0300

    dadant
    2134

---
+++ BalanceJamm/Assets/SmsControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmsControl : MonoBehaviour
+{
+    [SerializeField] private GameObject giftPrefab;
+    [SerializeField] private GameObject giftSpawnPosition;
+    [SerializeField] private GameObject TipPanel;
+    [SerializeField] private bool giftActive = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && giftActive == false)
+        {
+            TipPanel.SetActive(true);
+            Instantiate(giftPrefab, giftSpawnPosition.transform.position, Quaternion.identity);
+            giftActive = true;
+        }
+        if (collision.gameObject.CompareTag("Player") && giftActive == true)
+        {
+            TipPanel.SetActive(true);
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TipPanel.SetActive(false);
+        }
+    }
+}
