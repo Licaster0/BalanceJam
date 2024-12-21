e49271c01ededba529e094adb61e005ab9e535a6
Licaster0 <ahmet0434recep@gmail.com>
2024-12-21 20:56:44 +0300

    123

--- BalanceJamm/Assets/ESCMenuScript.cs
+++ BalanceJamm/Assets/ESCMenuScript.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ESCMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject pauseMenuUI;
+
     void Start()
     {
-        
+        SwitchTo(inGameUI);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SwitchWithKeyTo(pauseMenuUI);
+        }
+    }
+    public void SwitchTo(GameObject _menu)
+    {
+        if (_menu != null)
+        {
+            _menu.SetActive(true);
+        }
+
+        if (_menu == inGameUI)
+        {
+            PauseGame(false);
+        }
+        else
+        {
+            PauseGame(true);
+        }
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu)
+    {
+        if (_menu != null && _menu.activeSelf)
+        {
+            _menu.SetActive(false);
+            CheckForInGameUI();
+            return;
+        }
+
+        SwitchTo(_menu);
+    }
+
+    private void CheckForInGameUI()
+    {
+        /*
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).gameObject.activeSelf)
+            {
+                return;
+            }
+        }
+        */
+        SwitchTo(inGameUI);
+    }
+
+    public void PauseGame(bool _pause)
+    {
+        if (_pause)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
     }
 }
